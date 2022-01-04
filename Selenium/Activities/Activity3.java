4e108c6adaac21eabea60f80b2765c88404d22af
KomalShinde1 <97090873+KomalShinde1@users.noreply.github.com>
2022-01-04 15:25:07 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity3.java
@@ -0,0 +1,48 @@
+package selenium_Training;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+
+	public class Activity3 {
+
+		public static void main(String[] args) {
+			
+			
+			WebDriver driver = new FirefoxDriver();
+			driver.get("https://training-support.net/selenium/simple-form");
+			
+			String title = driver.getTitle();
+			
+			System.out.println("Title of the Page :"+title);
+			
+			WebElement fName = driver.findElement(By.id("firstName"));
+			
+			WebElement lName = driver.findElement(By.id("lastName"));
+			
+			WebElement email = driver.findElement(By.id("email"));
+			
+			WebElement contact = driver.findElement(By.id("number"));
+			
+			fName.sendKeys("Komal");
+			
+			lName.sendKeys("Shinde");
+			
+			email.sendKeys("xyz@gmail.com");
+			
+			contact.sendKeys("7219031705");
+			
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			
+			driver.close();
+			
+			
+			
+			
+			
+			
+
+		}
+
+	}
