84e1a50a1dbe7742edd98d8525f83ffddcd3dd99
KomalShinde1 <97090873+KomalShinde1@users.noreply.github.com>
2022-01-04 16:17:37 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,19 @@
+package activities;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		        Car swift =new Car();
+		        swift.make = 2014;
+		        swift.color = "Black";
+		        swift.transmission = "Manual";
+		   
+		        swift.displayCharacterstics();
+		        swift.accelerate();
+		        swift.brake();
+		    }
+
+		
+	}
+
+
---
+++ Java/Activities/pl.java
@@ -0,0 +1,12 @@
+package activities;
+
+public class pl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]=new int[5];
+System.out.print(arr);
+
+	}
+
+}
