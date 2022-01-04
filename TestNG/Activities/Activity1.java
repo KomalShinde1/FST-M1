7d2452d56e6b42acfe5524c0d4001368c4633f0d
KomalShinde1 <97090873+KomalShinde1@users.noreply.github.com>
2022-01-04 16:22:06 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity1.java
@@ -0,0 +1,44 @@
+package TestNGActivites;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Activity1 {
+  
+	private FirefoxDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		
+		driver = new FirefoxDriver();
+		driver.get("https://training-support.net");
+	}
+	
+	@Test
+	public void checkTitle() {
+		
+		String title= driver.getTitle();
+		System.out.println("Title of the page is :"+title);
+		Assert.assertEquals(title, "Training Support");
+		
+		driver.findElement(By.id("about-link")).click();
+		title= driver.getTitle();
+		System.out.println("Title of the new page is :"+title);
+		Assert.assertEquals(title, "About Training Support");
+	}
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+}
